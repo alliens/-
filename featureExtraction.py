1a5423f9aa921ec7d66bb897290a0de92b5ecfef
我系外星人 <alien@woxiwaixingrendeMacBook-Pro.local>
2020-02-20 13:55:49 +0800

    读取数据

--- featureExtraction.py
+++ featureExtraction.py
@@ -10,24 +10,26 @@ import pandas as pd
 from scipy.fftpack import fft
 import scipy.io as scio
 
-# def loadData_from_oneFile(filename):
-#     data=scio.loadmat('/Users/alien/Documents/f盘/毕设数据/12k驱动端/12k_Drive_End_B007_0_118.mat')
-    #   DE_time=data['X11']
-#     return wave
+def loadData_from_oneFile(filename):
+    data=scio.loadmat(filename)
+    for key in data.keys():
+        if key[-7:]=='DE_time':
+            wave=data[key]
+            break
+    return wave
 
-# def getFinal_data():
-#     file_list=glob.glob('/Users/alien/Documents/f盘/毕设数据/12k驱动端/'+'12k_Drive_End*')
-#     file_list=sorted(file_list)
-#     Data=[]
-#     for fileName in file_list:
-#         wave=loadData_from_oneFile(fileName)
-#         Data.append(wave[0])
-#     return Data
+def getFinal_data():
+    fileList=glob.glob('/Users/alien/Documents/f盘/毕设数据/12k驱动端/'+'12k_Drive_End*')
+    fileList=sorted(fileList)
+    Data=[]
+    for fileName in fileList:
+        wave=loadData_from_oneFile(fileName)
+        Data.append(wave)
+    return Data
 
-# data=hp.File('/Users/alien/Documents/f盘/毕设数据/12k驱动端/12k_Drive_End_B007_0_118.mat','r')
-# print(data)
-# data.close()
+def main():
+    Data=getFinal_data()
+    print(Data)
 
-data=scio.loadmat('/Users/alien/Documents/f盘/毕设数据/12k驱动端/12k_Drive_End_B007_1_119.mat')
-print(data.keys())
-
+if __name__ == "__main__":
+    main()

29dd8dfdc013792df3ea3d35b5e8e32706fe7dc8
我系外星人 <alien@woxiwaixingrendeMacBook-Pro.local>
2020-02-20 15:57:41 +0800

    添加生成频谱函数

--- featureExtraction.py
+++ featureExtraction.py
@@ -11,6 +11,10 @@ from scipy.fftpack import fft
 import scipy.io as scio
 
 def loadData_from_oneFile(filename):
+    '''
+    Parameters 
+    filename : the name of one file 
+    ''' 
     data=scio.loadmat(filename)
     for key in data.keys():
         if key[-7:]=='DE_time':
@@ -26,6 +30,26 @@ def getFinal_data():
         wave=loadData_from_oneFile(fileName)
         Data.append(wave)
     return Data
+
+def myspecfft(wave,fs):
+    '''
+    Parameters
+    ----------
+    wave : a one-dimension array of one file
+
+    fs : sampling frequence
+    '''
+    L=len(wave)
+    print(L)
+    wave=wave-np.mean(wave)
+    blmanharris=2.7875*np.blackman(L)
+    wave=wave*blmanharris
+    Wave=fft(wave)/L
+    #采样频率为12k，根据采样定理，频谱上只有0～6k是有效的，保险起见最好取12k/2.56
+    f=(fs/L)*np.array(range(1,int(L/2.56),1))
+    Af=2*abs(Wave[0:int(L/2.56)])
+    #返回值f为频率，Af为幅值
+    return f,Af
 
 def main():
     Data=getFinal_data()

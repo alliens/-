0c5d7f6b00741a91e970fc112b28b075370eaee6
我系外星人 <alien@woxiwaixingrendeMacBook-Pro.local>
2020-03-21 19:57:03 +0800

    add multi-class training modle

--- 1D_CNN_train_multi-Class.py
+++ 1D_CNN_train_multi-Class.py
@@ -1,21 +1,121 @@
 import csv
 import glob
-#正常数据标签0，滚动体故障标签1，内圈故障标签2，外圈故障标签3
-'''
-def getFileList(dirPath):
-    fileList=glob.glob(dirPath)
-    fileList=sorted(fileList)
-    return fileList
+import os
 
-fileList=getFileList('/Users/alien/Documents/f盘/毕设数据/小轴承实验台轴承故障数据_副本/外圈1故障数据/'+'2012*')
-for fileName in fileList:
-    with open (fileName,'r') as f:
-        data=f.readline()
-        data=list(data.split(' '))
-        data.pop()
-        data=[float(x) for x in data]
-        data=data[0:2000]
-    with open ('2000.csv','a+') as csv_f:
-        writer=csv.writer(csv_f)
-        writer.writerow(data)
+# import matplotlib
+# matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as scio
+from keras.layers import (Conv1D, Dense, Dropout, Embedding, Flatten,
+                          MaxPooling1D)
+from keras.models import Sequential
+from keras.utils import to_categorical
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+#正常数据标签0，滚动体故障标签1，内圈故障标签2，外圈故障标签3
+
+
+with open ('/Users/alien/Documents/d盘/python/本科毕设/小轴承实验台训练数据集.csv','r') as f:
+    reader=csv.reader(f)
+    Data=[]
+    for row in reader:
+        Data.append(row)
+    Data=np.array(Data)
+
 '''
+初始化参数
+'''
+batch_size=10
+epochs=20
+num_class=4 
+np.random.seed(6)
+
+x_vals=np.array([x[0:2000] for x in Data])
+y_vals=np.array([x[-1] for x in Data])
+
+
+train_indices = np.random.choice(len(x_vals), round(len(x_vals)*0.8), replace=False)
+test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
+x_vals_train = x_vals[train_indices]
+x_vals_test = x_vals[test_indices]
+y_vals_train = y_vals[train_indices]
+y_vals_test = y_vals[test_indices]
+
+y_vals_train=to_categorical(y_vals_train,num_class)
+y_vals_test=to_categorical(y_vals_test,num_class)
+'''
+将输入数据reshape符合Conv1D的input_shape
+'''
+x_vals_train=x_vals_train.reshape((x_vals_train.shape[0],x_vals_train.shape[1],1))
+x_vals_test=x_vals_test.reshape((x_vals_test.shape[0],x_vals_test.shape[1],1))
+
+# y_vals_train=y_vals_train.reshape((y_vals_train.shape[0],1))
+# # y_vals_test=y_vals_test.reshape((y_vals_test.shape[0],1))
+
+# print(np.shape(x_vals_train))
+# print(np.shape(y_vals_train))
+
+'''
+建立训练模型
+'''
+model=Sequential()
+#Convolution Layer (filter_shape=1*9,num_filter=60)
+model.add(Conv1D(60,9,padding='same',activation='relu',input_shape=(x_vals_train.shape[1],1)))
+#Subsampling Layer (filter_shape=1*4)
+model.add(MaxPooling1D(4))
+
+#Convolution Layer (filter_shape=1*9,num_filter=40)
+model.add(Conv1D(40,9,padding='same',activation='relu'))
+
+#Subsampling Layer (filter_shape=1*4)
+model.add(MaxPooling1D(4))
+
+#Convolution Layer (filter_shape=1*9,num_filter=40)
+model.add(Conv1D(40,9,padding='same',activation='relu'))
+
+#将最后一层卷积层的扁平化后再输入到全连接网络
+model.add(Flatten())
+#rate=0.5表示丢弃的比例，将50%的数据置为0，有助于防止过拟合
+model.add(Dropout(0.3))
+
+#Fully Connected MLP Layer (20 neurons)
+model.add(Dense(20,activation='relu'))
+
+#Output Layer (only 1 neuron for 2 classes)
+model.add(Dense(4,activation='sigmoid'))
+
+'''
+编译训练模型
+'''
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+
+History=model.fit(x_vals_train,y_vals_train,
+            batch_size=batch_size,
+            validation_data=(x_vals_test,y_vals_test),
+            epochs=epochs,verbose=2)
+
+score=model.evaluate(x_vals_test,y_vals_test,
+            batch_size=batch_size)
+
+
+
+#画结果图（train_loss、test_loss、train_acc、test_acc)
+N=np.arange(1,epochs+1)
+title='Training Loss and Accuracy on CWRU dataset(12k-DE)'
+
+plt.style.use('ggplot')
+# plt.figure()
+plt.plot(N,History.history['loss'],label='train_loss')
+plt.plot(N,History.history['val_loss'],label='test_loss')
+plt.plot(N,History.history['accuracy'],label='train_acc')
+plt.plot(N,History.history['val_accuracy'],label='test_acc')
+plt.title(title)
+plt.xlabel('Epoch')
+plt.ylabel('Loss/Accuracy')
+plt.legend()
+plt.show()
